e4c73526bf1dcad609b00e5a5a86f0847cb26f79
Musooo <nicoloblack2@gmail.com>
2024-12-05 15:56:59 +0100

    commit some initial stuff i have to change to linux

--- client.c
+++ client.c
@@ -1,9 +1,20 @@
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 20001
 
 
-int main(int argc, char *argv[]){
-    
 
+int main(int argc, char *argv[]){ // [1] name [2] age
+    int status, valread, client_fd;
+    struct sockaddr_in serv_addr;
+
+    char* message = malloc(strlen(argv[1])+strlen(argv[2])+1);
+
+    printf("name: %s\n", name);
+    printf("age: %d\n", age);
+    free(message);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,4 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PORT 20002
 
+int totj = 0;
+int totm = 0;
+int tots = 0;
+
+int pow(int n,int b){
+    if (b==0){
+        return 1;
+    }
+    return n*pow(n,b-1);
+}
+
+int stringtoint(char *arr,int pos,int index){
+    if (pos==0){
+        return 0;
+    }
+
+    return (arr[index]-48)*pow(10,pos-1)+stringtoint(arr,pos-1,index+1);
+}
 
 int main(int argc, char *argv[]){
 
