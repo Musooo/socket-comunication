c40daad539a638cdcf7c3d8c43b63a4ba239dd48
Musooo <nicoloblack2@gmail.com>
2024-12-05 16:16:45 -0500

    client and server communicate

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 20001
+#define PORT 20002
 
 int len(char *string) {
     if (*string == '\0') {
@@ -23,8 +23,40 @@ int main(int argc, char *argv[]){ // [1] name [2] age
     int status, valread, client_fd;
     struct sockaddr_in serv_addr;
 
-    char* message = malloc(len(argv[1])+len(argv[2])+1);
+    char *message = malloc(len(argv[1])+len(argv[2])+1);
+    char *hello = "Hello from client";
+    char buffer[1024] = { 0 };
 
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if ((status
+         = connect(client_fd, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    send(client_fd, hello, len(hello), 0);
+    printf("Information sent to the server\n");
+    valread = read(client_fd, buffer,
+                   1024 - 1);
+    printf("%s\n", buffer);
+    
+    close(client_fd);
     free(message);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,6 +1,14 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 #define PORT 20002
+
+#ifndef SO_REUSEPORT
+#define SO_REUSEPORT 15
+#endif
 
 int totj = 0;
 int totm = 0;
@@ -35,7 +43,54 @@ int stringtoint(char *arr,int pos,int index){
 }
 
 int main(int argc, char *argv[]){
+        int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
 
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // attaching socket to the port 20002
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // attaching socket to the port 20002
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    while(1){
+        if (listen(server_fd, 10) < 0) {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read(new_socket, buffer, 1024 - 1); // subtract 1 for the '\0'
+        buffer[valread] = '\0'; // Ensure null-terminated string
+        printf("%s\n", buffer);
+        send(new_socket, hello, strlen(hello), 0);
+        printf("Hello message sent\n");
+
+        // closing the connected socket
+        close(new_socket);
+    }
+
+    // closing the listening socket
+    close(server_fd);
     return 0;
 }
