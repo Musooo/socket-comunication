02133ca1473337fd8b60025beef9447edd26ab04
musooo <nicoloblack2@gmail.com>
2024-12-12 20:57:28 +0100

    server receive user name

--- chat/client.c
+++ chat/client.c
@@ -3,18 +3,25 @@
 #include <sys/types.h> 
 #include <arpa/inet.h> 
 #include <sys/socket.h> 
-#include<netinet/in.h> 
-#include<unistd.h> 
-#include<stdlib.h> 
+#include <netinet/in.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include "func.h"
 
 #define IPADDRESS "127.0.0.1"
 #define PORT 1313
 
 
-int main() 
-{    
+int main(int argc, char **argv) {
+    if (argc != 2)
+        return -1;
+    
     char buffer[100]; 
-    char *message = "Hello Server"; 
+    char *message = malloc(len(argv[1])+2); 
+    strcpy(message, "0"); //init message
+    strcat(message, argv[1]);
+
     int sockfd, n; 
     struct sockaddr_in servaddr; 
       
@@ -28,8 +35,7 @@ int main()
     sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
       
     // connect to server 
-    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
-    { 
+    if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) { 
         printf("\n Error : Connect Failed \n"); 
         exit(0); 
     } 
@@ -38,11 +44,16 @@ int main()
     // no need to specify server address in sendto 
     // connect stores the peers IP and port 
     sendto(sockfd, message, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
-      
-    // waiting for response 
-    recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
-    puts(buffer); 
-  
+    free(message);
+
+
+    while (1){
+        // waiting for response 
+        recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
+        puts(buffer);
+    }
+ 
+
     // close the descriptor 
     close(sockfd); 
 } 
--- chat/server.c
+++ chat/server.c
@@ -11,37 +11,72 @@
 #include <termios.h>
 #include <fcntl.h>
 
+
 #define PORT 1313
-struct sockaddr_in client;
+
+typedef struct {
+    struct sockaddr_in client;
+    char *name;
+}user;
 
 
-int main() {    
-        char buffer[100]; 
-        char *message = "Hello Client"; 
-        int listenfd, len; 
-        struct sockaddr_in servaddr, cliaddr; 
-        bzero(&servaddr, sizeof(servaddr)); 
+user *userarr = NULL;
+int size = 0;
 
-        // Create a UDP Socket 
-        listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-        servaddr.sin_port = htons(PORT); 
-        servaddr.sin_family = AF_INET;  
-        while (1){
-                // bind server address to socket descriptor 
-                bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
+void alloclient(struct sockaddr_in client, char *name){
+    userarr = (user* ) realloc(userarr, (size + 1) * sizeof(user));
+    userarr[size].client = client;
+    userarr[size].name = strdup(name);
+    size++;
+}
 
-                //receive the datagram 
-                len = sizeof(cliaddr); 
-                int n = recvfrom(listenfd, buffer, sizeof(buffer), 
-                        0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
-                buffer[n] = '\0'; 
-                puts(buffer); 
-                        
-                // send the response 
-                sendto(listenfd, message, 1000, 0, 
-                        (struct sockaddr*)&cliaddr, sizeof(cliaddr)); 
+void dealloc(){
+    for (int i = 0; i < size; i++) {
+        free(userarr[i].name);
+    }
+    free(userarr);
+}
+
+int count = 0;
+
+void *countt(void *ptr){
+    count++;
+}
+
+int main() {
+    pthread_t thread_id;
+    char buffer[100]; 
+    char *message = "Hello Client"; 
+    int listenfd, len; 
+    struct sockaddr_in servaddr, cliaddr; 
+    bzero(&servaddr, sizeof(servaddr)); 
+
+    // Create a UDP Socket 
+    listenfd = socket(AF_INET, SOCK_DGRAM, 0);         
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_family = AF_INET;
+    // bind server address to socket descriptor 
+    bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
+    pthread_create(&thread_id, NULL, countt, NULL);
+    while (1){
+        //receive the datagram 
+        len = sizeof(cliaddr); 
+        int n = recvfrom(listenfd, buffer, sizeof(buffer), 
+            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
+        
+        buffer[n] = '\0'; 
+        puts(buffer); 
+        if (buffer[0]=='0'){
+            memmove(buffer, buffer + 1, strlen(buffer));
+            alloclient(cliaddr,buffer);
         }
+        
+        // send the response 
+        sendto(listenfd, message, 1000, 0, 
+            (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+    }
 
-        return 0;
-} 
+    dealloc();
+    return 0;
+}
