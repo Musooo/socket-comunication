cce94cf00a4a33103dec36b2654e748fae215bbe
Musooo <nicoloblack2@gmail.com>
2024-12-13 15:17:39 -0500

    client send message to server receive

--- chat/client.c
+++ chat/client.c
@@ -7,16 +7,74 @@
 #include <unistd.h> 
 #include <stdlib.h> 
 #include <string.h>
+#include <termios.h>
+#include <pthread.h>
+
 #include "func.h"
 
 #define IPADDRESS "127.0.0.1"
 #define PORT 1313
 
+char currentchar;
+struct termios orig_termios;
+char messaggio[100];
+
+typedef struct {
+    struct sockaddr_in servaddr;
+    int sockfd;
+    /* data */
+}threadlistenersockets;
+
+
+void configure_terminal() {
+    struct termios info;
+    tcgetattr(0, &orig_termios);  /* salva le impostazioni originali del terminale */
+    tcgetattr(0, &info);         /* get current terminal attirbutes; 0 is the file descriptor for stdin */
+    info.c_lflag &= ~ICANON;      /* disable canonical mode */
+    info.c_lflag &= ~ECHO;
+    info.c_cc[VMIN] = 1;          /* wait until at least one keystroke available */
+    info.c_cc[VTIME] = 0;         /* no timeout */
+    tcsetattr(0, TCSANOW, &info); /* set immediately */
+}
+
+void resetterminal(){
+    tcsetattr(0, TCSANOW, &orig_termios);
+}
+
+void *keylistener(void *ptr){
+    threadlistenersockets tsockets = *(threadlistenersockets*)ptr; 
+
+    while (1){
+        currentchar = getchar();
+        switch (currentchar){
+        case '2':
+            resetterminal();
+            fgets(messaggio, sizeof(messaggio), stdin);
+            messaggio[strcspn(messaggio, "\n")] = 0;
+
+            char new_message[1024];
+            snprintf(new_message, sizeof(new_message), "42%s", messaggio);
+
+            sendto(tsockets.sockfd, new_message, strlen(new_message), 0, (struct sockaddr*)&tsockets.servaddr, sizeof(tsockets.servaddr)); 
+            
+            printf("you: %s\n", new_message);
+            configure_terminal();
+            break;
+        case '3':
+            exit(0);
+            break;
+        default:
+            break;
+        }
+            
+    }
+}
+
 
 int main(int argc, char **argv) {
     if (argc != 2)
         return -1;
-    
+    pthread_t keylt_id;
     char buffer[100]; 
     char *message = malloc(len(argv[1])+2); 
     strcpy(message, "0"); //init message
@@ -46,7 +104,12 @@ int main(int argc, char **argv) {
     sendto(sockfd, message, 1000, 0, (struct sockaddr*)NULL, sizeof(servaddr)); 
     free(message);
 
+    threadlistenersockets tsockets;
+    tsockets.servaddr = servaddr;
+    tsockets.sockfd = sockfd;
 
+    configure_terminal();
+    pthread_create(&keylt_id, NULL, keylistener, (void *) &tsockets);
     while (1){
         // waiting for response 
         recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)NULL, NULL); 
--- chat/server.c
+++ chat/server.c
@@ -19,7 +19,7 @@ typedef struct {
     char *name;
 }user;
 
-
+pthread_mutex_t lock;
 user *userarr = NULL;
 int size = 0;
 
@@ -43,6 +43,10 @@ void *countt(void *ptr){
     count++;
 }
 
+// void *sendmsg(void *ptr){
+
+// }
+
 int main() {
     pthread_t thread_id;
     char buffer[100]; 
@@ -59,22 +63,36 @@ int main() {
     // bind server address to socket descriptor 
     bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
     pthread_create(&thread_id, NULL, countt, NULL);
-    while (1){
-        //receive the datagram 
+    while (1) {
+        // receive the datagram 
         len = sizeof(cliaddr); 
-        int n = recvfrom(listenfd, buffer, sizeof(buffer), 
-            0, (struct sockaddr*)&cliaddr,&len); //receive message from server 
+        int n = recvfrom(listenfd, buffer, sizeof(buffer) - 1, 0, (struct sockaddr*)&cliaddr, &len); // receive message from server 
         
-        buffer[n] = '\0'; 
-        puts(buffer); 
-        if (buffer[0]=='0'){
-            memmove(buffer, buffer + 1, strlen(buffer));
-            alloclient(cliaddr,buffer);
+        if (n > 0) {
+            buffer[n] = '\0'; // Ensure null-terminated string
+            puts(buffer); 
+            if (buffer[0] == '0') {
+                //printf("we");
+                memmove(buffer, buffer + 1, strlen(buffer));
+                //printf("ok");
+                alloclient(cliaddr, buffer);
+                sendto(listenfd, buffer, strlen(buffer), 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+            } else if (buffer[0] == '4' && buffer[1] == '2') {
+                //printf("we\n");
+                memmove(buffer, buffer + 2, strlen(buffer) - 1);
+                //printf("done\n");
+                // manda il messaggio a tutti gli altri client
+                for (int i = 0; i < sizeof(userarr) / sizeof(userarr[0]); i++) {
+                    if (userarr[i].client.sin_addr.s_addr != cliaddr.sin_addr.s_addr || 
+                        userarr[i].client.sin_port != cliaddr.sin_port) {
+                        sendto(listenfd, buffer, strlen(buffer), 0, (struct sockaddr*)&userarr[i].client, sizeof(userarr[i].client));                        
+                    }
+                }
+            }else {
+                sendto(listenfd, message, strlen(message), 0, (struct sockaddr*)&cliaddr, sizeof(cliaddr));
+            }
+            //printf("%c\n", buffer[0]);
         }
-        
-        // send the response 
-        sendto(listenfd, message, 1000, 0, 
-            (struct sockaddr*)&cliaddr, sizeof(cliaddr));
     }
 
     dealloc();
